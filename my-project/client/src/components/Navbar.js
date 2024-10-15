ebe2e9303aec88f9b7ec1853f60a98e3a31264bd
Vishal Vaddar <117060432+vishalvaddar@users.noreply.github.com>
2024-10-15 16:11:26 +0530

    updated navbar

--- my-project/client/src/components/Navbar.js
+++ my-project/client/src/components/Navbar.js
@@ -1,21 +1,32 @@
-// Navbar.js
 import React from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <ul>
-        <li><a href="/">Dashboard</a></li>
-        <li><a href="/view-applications">View Applications</a></li>
-        <li><a href="/bulk-upload">Bulk Upload Applications</a></li>
-        <li><a href="/shortlisting-process">Shortlisting Process</a></li>
-        <li><a href="/screening-tests">Screening Tests/Interviews</a></li>
-        <li><a href="/results-management">Results Management</a></li>
-        <li><a href="/reports">Reports & Analytics</a></li>
-        <li><a href="/user-management">User Management</a></li>
-        <li><a href="/settings">Settings</a></li>
-        <li><a href="/help-support">Help & Support</a></li>
+      <ul className="navbar-nav">
+        <li className="nav-item"><a href="/">Dashboard</a></li>
+        <li className="nav-item dropdown">
+          <a href="/upload-applications" className="dropbtn">Upload Applications</a>
+          <div className="dropdown-content">
+            <a href="/new-application">New Application</a>
+            <a href="/bulk-upload-applications">Bulk upload Applications</a>
+          </div>
+        </li>
+        <li className="nav-item"><a href="/view-applications">View Applications</a></li>
+        <li className="nav-item dropdown">
+          <a href="/shortlisting" className="dropbtn">Shortlisting</a>
+          <div className="dropdown-content">
+            <a href="/create-shortlisting-criteria">Create Shortlisting Criteria</a>
+            <a href="/generate-shortlist">Generate Shortlist</a>
+          </div>
+        </li>
+        <li className="nav-item"><a href="/screening-tests">Screening Tests/Interviews</a></li>
+        <li className="nav-item"><a href="/results-management">Results Management</a></li>
+        <li className="nav-item"><a href="/reports">Reports & Analytics</a></li>
+        <li className="nav-item"><a href="/user-management">User Management</a></li>
+        <li className="nav-item"><a href="/settings">Settings</a></li>
+        <li className="nav-item"><a href="/help-support">Help & Support</a></li>
       </ul>
     </nav>
   );
